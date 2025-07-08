da54f5d7ba0084336e8f5965729b6db391545658
Ned_Frien <71213583+sirawichtang@users.noreply.github.com>
2025-07-08 05:38:05 +0000

    commit

--- Lab-1/ex02.c
+++ Lab-1/ex02.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    printf("My name is Ryu\n");
+    printf("My name is %s\n", "Ryu");
     printf("I am %d years old.\n", 18);
-    printf("I am %d cm ish tall\n", 170);
+    printf("I am %.2f cm ish tall\n", 170.00);
 }
